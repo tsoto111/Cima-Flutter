125cf9802c54a48b69af4e8751f38ab20db0feac
Tavo <tsoto111@gmail.com>
2020-03-09 23:55:41 -0500

    Added init life cycle method to loading screen and exception handler

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -7,30 +7,33 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  // [Tavo] - Life Cycle Method: On Component Init
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getLocation();
+  }
+
   // [Tavo] - We have to tell this function that it is going to be asynchronous
   // by adding `async` key word after the function's definition...
   void getLocation() async {
-    // [Tavo] - We know that the Geolocator() function call is asynchronous because it
-    // uses the `await` key word before its usage...
-    Position position = await Geolocator().getCurrentPosition(
-      desiredAccuracy: LocationAccuracy
-          .low, // low, to use a minimal amount of device resources
-    );
-    print(position);
+    try {
+      // [Tavo] - We know that the Geolocator() function call is asynchronous because it
+      // uses the `await` key word before its usage...
+      Position position = await Geolocator().getCurrentPosition(
+        desiredAccuracy: LocationAccuracy
+            .low, // low, to use a minimal amount of device resources
+      );
+      print(position);
+    } catch (error) {
+      print(error);
+    }
   }
 
+  // [Tavo] Build gets called every time the screen gets rebuilt...
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-            getLocation();
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
