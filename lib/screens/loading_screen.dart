fe11047029f165ee1fdec4afb5a264ff734ed0d7
Tavo <tsoto111@gmail.com>
2020-03-10 23:25:53 -0500

    Importing flutter http package and creating my first http get request

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -22,6 +23,19 @@ class _LoadingScreenState extends State<LoadingScreen> {
     await currentLocation.getCurrentLocation();
     print(currentLocation.latitude);
     print(currentLocation.longitude);
+    getData();
+  }
+
+  Future<void> getData() async {
+    http.Response currentWeather = await http.get(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+    print(currentWeather.statusCode);
+    if (currentWeather.statusCode == 200) {
+      String data = currentWeather.body;
+      print(currentWeather.body);
+    } else {
+      print(currentWeather.statusCode);
+    }
   }
 
   // [Tavo] Build gets called every time the screen gets rebuilt...
