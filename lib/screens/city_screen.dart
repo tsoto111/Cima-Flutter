7b2ffef38ce6dc48a73ac489a1c948559f9abcdd
Tavo <tsoto111@gmail.com>
2020-03-14 01:09:17 -0500

    Finishing get weather data by city logic

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -8,6 +8,7 @@ class CityScreen extends StatefulWidget {
 }
 
 class _CityScreenState extends State<CityScreen> {
+  String cityName;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,12 +43,14 @@ class _CityScreenState extends State<CityScreen> {
                   ),
                   decoration: kTextFieldDecoration,
                   onChanged: (value) {
-                    print(value);
+                    cityName = value;
                   },
                 ),
               ),
               FlatButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.pop(context, cityName);
+                },
                 child: Text(
                   'Get Weather',
                   style: kButtonTextStyle,
--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -5,6 +5,13 @@ const apiKey = '8166d2071f21e28191224c569a1af373';
 const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
 
 class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName) async {
+    var url = '$openWeatherMapURL/?q=$cityName&appid=$apiKey&units=metric';
+    NetworkHelper networkHelper = NetworkHelper(url);
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
   Future<dynamic> getLocationWeather() async {
     Location currentLocation = new Location();
     await currentLocation.getCurrentLocation();
